6180b2009e752d768cf7c6546b69dfdd3ff7f668
Kanav Gupta <kanav0610@gmail.com>
2024-05-28 15:16:41 +0530

    update

--- GPU-MPC/ext/sytorch/scripts/all-cpu-benchmarks.py
+++ GPU-MPC/ext/sytorch/scripts/all-cpu-benchmarks.py
@@ -11,16 +11,17 @@ benchmarks = [
     'gptneo'
 ]
 
-logfile = open("log.log", 'a')
+logfile1 = open("log1.log", 'a')
+logfile2 = open("log2.log", 'a')
 
 def run_seq(cmd):
-    p = subprocess.Popen(cmd, shell=True, stdout=logfile, stderr=logfile)
+    p = subprocess.Popen(cmd, shell=True, stdout=logfile1, stderr=logfile1)
     p.wait()
 
 
 def run_par(cmd1, cmd2):
-    p1 = subprocess.Popen(cmd1, shell=True, stdout=logfile, stderr=logfile)
-    p2 = subprocess.Popen(cmd2, shell=True, stdout=logfile, stderr=logfile)
+    p1 = subprocess.Popen(cmd1, shell=True, stdout=logfile1, stderr=logfile1)
+    p2 = subprocess.Popen(cmd2, shell=True, stdout=logfile2, stderr=logfile2)
     p1.wait()
     p2.wait()
 
@@ -29,7 +30,7 @@ for b in benchmarks:
     print("[+] --- compiling...")
     run_seq('make benchmark-' + b)
     print("[+] --- running dealer...")
-    run_seq(f'./benchmark-{b} 1')
+    run_seq(f'OMP_NUM_THREADS=4 ./benchmark-{b} 1')
     print("[+] --- running online phase...")
     run_par(f'OMP_NUM_THREADS=4 ./benchmark-{b} 2', f'OMP_NUM_THREADS=4 ./benchmark-{b} 3')
 
@@ -71,4 +72,5 @@ for b in benchmarks:
     print("[+] --- online time = " + str(total_time/1000.0) + " s")
     print("[+] --- online comm = " + str(total_comm/1024.0) + " GB")
 
-logfile.close()
+logfile1.close()
+logfile2.close()
