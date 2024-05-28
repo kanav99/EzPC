b6575c6a998796d58fe916b791a2ef67ff50768c
Kanav Gupta <kanav0610@gmail.com>
2024-05-28 14:02:42 +0530

    update

--- GPU-MPC/ext/sytorch/scripts/all-cpu-benchmarks.py
+++ GPU-MPC/ext/sytorch/scripts/all-cpu-benchmarks.py
@@ -16,8 +16,8 @@ def run_seq(cmd):
 
 
 def run_par(cmd1, cmd2):
-    p1 = subprocess.Popen(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
-    p2 = subprocess.Popen(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    p1 = subprocess.Popen(cmd1, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    p2 = subprocess.Popen(cmd2, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     p1.wait()
     p2.wait()
 
@@ -27,5 +27,6 @@ for b in benchmarks:
     run_seq('make benchmark-' + b)
     print("[+] --- running dealer...")
     run_seq(f'./benchmark-{b} 1')
+    print("[+] --- running online phase...")
     run_par(f'OMP_NUM_THREADS=4 ./benchmark-{b} 2', f'OMP_NUM_THREADS=4 ./benchmark-{b} 3')
 
